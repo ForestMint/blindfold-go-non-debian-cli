cb73470be66f131263b6a5e2d10a910be3dd225f
Nicolas Desdames <n.desdames@gmail.com>
2024-05-01 20:58:31 +0200

    stone adding moves are well detected and proceeded

--- cli.py
+++ cli.py
@@ -1,10 +1,7 @@
-
-
 import speech_recognition as sr
 recognizer = sr.Recognizer()
 
 import json
-
 
 import requests
 
@@ -14,8 +11,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-
+###############################################
+##########    CREATE AUDIO FUNCTIONS    #######
+###############################################
 
 def record_audio():
     with sr.Microphone() as source:
@@ -32,83 +30,87 @@ def recognize_speech(audio):
     except sr.RequestError:
         print("Sorry, there was an error processing your request.")
 
-
-
-
-
-
-
-
-
-
-game_id=None
-
+###############################################
+##########    CREATE GAME FUNCTIONS    ########
+###############################################
 
 def create_game(size,handicap,komi):
-    r= requests.get('http://127.0.0.1:5000/create_game', data={'size': size, 'handicap':handicap, 'komi':komi})
+    r= requests.get('http://127.0.0.1:5000/create_game', params={'size': size, 'handicap':handicap, 'komi':komi})
 
-    return r.json()["game_id"]
+    return r.json()["game_manager_uuid"]
 
-
-def is_blindfolded_player_black(game_id):
+def is_blindfolded_player_black(game_manager_uuid):
     r= requests.get('http://127.0.0.1:5000/is_blindfolded_player_black/')
     return True
 
-def request_move_from_server(game_id):
-    r= requests.get('http://127.0.0.1:5000/request_move_from_server/')
+def request_move_from_server(game_manager_uuid):
+    r= requests.get('http://127.0.0.1:5000/request_ply_from_engine/')
     return "D4" 
 
-def is_game_over(game_id):
+def is_game_over(game_manager_uuid):
     r= requests.get('http://127.0.0.1:5000/is_game_over/')
     return False
 
-def play_move(game_id, move):
-    r= requests.get('http://127.0.0.1:5000/submit_move/') 
+def play_move(game_manager_uuid, move):
+    r= requests.get(url='http://127.0.0.1:5000/play_move',params={'game_manager_uuid': game_manager_uuid,"move":move}) 
 
-def is_move(game_id, move):
-    r= requests.get('http://127.0.0.1:5000/is_move/')
-    return False  
 
-def is_playable_move(game_id, move):
+
+def is_move(game_manager_uuid, candidate_speech):
+    r= requests.get(url='http://127.0.0.1:5000/is_move', params={'game_manager_uuid': game_manager_uuid, 'candidate_speech':candidate_speech})
+    return r.json()["result"]
+
+'''
+def is_playable_move(game_manager_uuid, move):
     r= requests.get('http://127.0.0.1:5000/is_playable_move/')
     return True  
+'''
 
+def submit_pass(game_manager_uuid):
+    pass
+
+def resign_game(game_manager_uuid):
+    pass
+
+def request_undo(game_manager_uuid):
+    pass
+
+
+
+##############################################################
+##########    CREATE GAME AND INITIALIZE VARIABLES    ########
+##############################################################
+
+#game_id=None
 size=int(config['GOBAN']['size'])
 handicap=int(config['HANDICAP']['number_of_stones'])
 komi=float(config['RULES']['komi'])
-game_id=create_game(size,handicap,komi)
-
+game_manager_uuid=create_game(size,handicap,komi)
 
 r = sr.Recognizer()
 mic = sr.Microphone()
 sr.Microphone.list_microphone_names()
 
-blindfolded_player_is_black=is_blindfolded_player_black(game_id)
+blindfolded_player_is_black=is_blindfolded_player_black(game_manager_uuid)
 label_dict={True:"black", False:"white"}
 #os.system("say 'Blindfolded player plays with the "+label_dict[blindfolded_player_is_black]+" stones !'") 
 #os.system("say 'Game started !'")
 
 if not blindfolded_player_is_black:
-    move=request_move_from_server(game_id)
+    move=request_move_from_server(game_manager_uuid)
     #os.system("say 'White plays "+move+" !'")
 
+##########################################
+##########    RUN PLAYING LOOP    ########
+##########################################
 
-while(is_game_over(game_id)==False):
-
-    
-
-
-
-
+while(is_game_over(game_manager_uuid)==False):
 
     result=""
+    print(result)
 
-
-
-
-
-    while(is_move(game_id,result)==False):
-        print(result)
+    # while the speech is not a playable move
+    while(is_move(game_manager_uuid,result)==False):
         with mic as source:
             r.adjust_for_ambient_noise(source)
             audio = r.listen(source)
@@ -119,30 +121,23 @@ while(is_game_over(game_id)==False):
             # speech was unintelligible
             result=""
 
-
-
-    if is_playable_move(game_id,move):
-        play_move(game_id,move)
-        if result == "pass":
-            #os.system("say '"+label_dict[blindfolded_player_is_black]+" passes !'")
-            pass
-        elif result == "resign":
-            #os.system("say '"+label_dict[blindfolded_player_is_black]+" resigns !'") 
-            pass
-        else:
-            #os.system("say '"+label_dict[blindfolded_player_is_black]+" plays "+result+" !'") 
-            pass
+    # when the speech is a playable move
+    if result in ["pass","Pass"]:
+        submit_pass(game_manager_uuid)
+    elif result in ["resign","Resign"]:
+        resign_game(game_manager_uuid)
+    elif result in ["undo","Undo"]:
+        request_undo(game_manager_uuid)
     else:
-        #os.system("say 'This move can't be played !'")
-        pass
+        play_move(game_manager_uuid,result)
+    
 
-    if is_game_over(game_id):
+    if is_game_over(game_manager_uuid):
         break
     else:
-        move_from_AI=request_move_from_server(game_id)
+        move_from_AI=request_move_from_server(game_manager_uuid)
         #os.system("say '"+label_dict[blindfolded_player_is_black==False]+" plays "+move_from_AI+" !'")
 
 
-    print(result)
 
 #os.system("say 'Thanks for the game !'")
